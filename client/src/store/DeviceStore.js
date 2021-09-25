9f8b7415e32dddfe48acf555cdd66b27010380e2
Svyatogub <svyatogubzemlyanichkeen@gmail.com>
2021-09-25 20:09:44 +0300

    golova kipit

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -5,6 +5,8 @@ export default class DeviceStore {
         this._types = [
             {id: 1, name: 'Холодильники'},
             {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Навбуки'},
+            {id: 4, name: 'Телевизоры'},
         ]
         this._brands = [
             {id: 1, name: 'Samsung'},
@@ -16,6 +18,7 @@ export default class DeviceStore {
             {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img:`https://estore.ua/media/catalog/product/cache/8/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-pacific-blue_3_.jpeg`},
             {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img:`https://estore.ua/media/catalog/product/cache/8/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-12-pro-pacific-blue_3_.jpeg`}
         ]
+        this._selectedType = {}
         makeAutoObservable(this)
     }
 
@@ -28,6 +31,9 @@ export default class DeviceStore {
     setDevices(devices) {
         this._devices = devices
     }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
 
     get types() {
         return this._types
@@ -37,5 +43,8 @@ export default class DeviceStore {
     }
     get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
     }
 }
